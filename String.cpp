c6b184050f21facbebe5b8cf952a6380ef1d50c7
pratham0709 <109234076+pratham0709@users.noreply.github.com>
2022-08-15 17:39:27 +0530

    Create String.cpp

---
+++ String.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main(){
+    
+    string s1; //Always define like ""
+    char c = 'a'; // Always in single ''
+    
+    
+    cout<<"Please enter some text : "<<endl;
+    getline(cin, s1);
+    
+    cout<<"output is : "<<s1<<endl;
+    
+    cout<<"second letter of string s1 is : "<<s1[1]<<endl;
+    
+    cout<<"lenght of the given string is : "<<s1.length()<<endl;
+    
+    reverse(s1.begin(), s1.end());
+    
+    cout<<"Revesred string is : "<<s1<<endl;
+    
+}
+
+
+/* OUTPUT.....
+Please enter some text : 
+pratham
+output is : pratham
+second letter of string s1 is : r
+lenght of the given string is : 7
+Revesred string is : mahtarp
+
+*/
