1afd6803a5d43672620a0472e648f3218ca9990e
pratham0709 <109234076+pratham0709@users.noreply.github.com>
2022-08-15 17:42:06 +0530

    Create string_01.cpp

---
+++ string_01.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main(){
+    
+    string str1 = "12";
+    string str2 = "51";
+    
+    string s;
+    
+    s = str1+str2; // 1251 0r 63 ??
+    
+    cout<<"s value is : "<<s<<endl;
+    
+    string a = "hello I am Pra";
+    
+    a = a + "tham"; 
+    
+    cout<<"a value is : "<<a<<endl;
+    
+ 
+    
+    
+}
+
+/* OUTPUT....
+s value is : 1251
+a value is : hello I am Pratham
+*/
